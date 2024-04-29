7515f7d7cdd0949aebcea9a777e7f495c34a477e
James McLean <jm2022@hw.ac.uk>
2024-04-29 09:07:03 +0100

    The Parser (for real this time)
    - Messed up that last commit message then messed up a trying to overwrite it. Now messing up again by accidentally calling same git function rather then the build function
    - I have now created the parser file but it is empty

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod lexer;
 
 fn main() {
-    let test_code = String::from("mew mew mrrp chrrp nyaa *push off* *swipe* *swipe into littertray* *chomp* meow meow? *pat* *pounce* *scratch* mraow yowl *paw* purr hiss *stop pawing* *zoomies* *eepy*");
+    let test_code = String::from("mew mew mrrp chrrp nyaa *push off* *swipe* 
+    *swipe into littertray* *chomp* meow meow? *pat* *pounce* *scratch* mraow yowl *paw* purr hiss *stop pawing* *zoomies* *eepy*");
     lexer::lex_program(test_code);
 }
